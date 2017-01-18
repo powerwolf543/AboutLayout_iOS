4316fe9980577f2ee0db2bce4b9a2634550e7e9b
NixonShih <powerwolf543@gmail.com>
2017-01-18 16:25:16 +0800

    develop init

---
+++ AboutLayout/SampleScaleOneSuccessViewController.h
@@ -0,0 +1,13 @@
+//
+//  SampleScaleOneSuccessViewController.h
+//  AboutLayout
+//
+//  Created by NixonShih on 2017/1/17.
+//  Copyright © 2017年 Nixon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SampleScaleOneSuccessViewController : UIViewController
+
+@end
---
+++ AboutLayout/UIViewController+LabelTextFitScreen.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+LabelTextFitScreen.h
+//  AboutLayout
+//
+//  Created by NixonShih on 2017/1/17.
+//  Copyright © 2017年 Nixon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (LabelTextFitScreen)
+- (void)changeLabelFontSizeWithScaleRate:(CGFloat)scaleRate;
+@end
