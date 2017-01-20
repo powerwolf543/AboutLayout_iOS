1fbc0fbc9b08bbc799fecd6cda4ecd97c298d4bc
NixonShih <powerwolf543@gmail.com>
2017-01-20 11:37:44 +0800

    1.完成ObjC的部分 2.剛創建Swift的部分

--- AboutLayout_ObjC/AboutLayout/UIViewController+TextFitScreen.h
+++ AboutLayout_ObjC/AboutLayout/UIViewController+TextFitScreen.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface UIViewController (TextFitScreen)
+/** 置換當前ViewController內的字體大小，針對 UILabel 以及 UIButton。 */
 - (void)changeFontSizeWithScalingRate:(CGFloat)scalingRate;
 @end
