09e42787286beb29291251454734ebae160288a3
NixonShih <powerwolf543@yahoo.com.tw>
2017-01-28 22:01:59 +0800

    增加 objC coding UI 範例

---
+++ AboutLayout_ObjC/AboutLayout/ProgrammaticallyViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProgrammaticallyViewController.h
+//  AboutLayout
+//
+//  Created by NixonShih on 2017/1/28.
+//  Copyright © 2017年 Nixon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProgrammaticallyViewController : UIViewController
+
+@end
